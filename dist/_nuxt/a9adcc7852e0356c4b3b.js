29abcf0e0f531daf6eada1f37ca2f02adaca317e
konny0311 <konotorie@gmail.com>
2020-01-13 15:02:12 +0900

    edit gitignore

---
+++ dist/_nuxt/a9adcc7852e0356c4b3b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{267:function(n,e,t){"use strict";t.r(e);var c={components:{}},o=t(59),component=Object(o.a)(c,(function(){var n=this.$createElement;return(this._self._c||n)("div",[this._v("\n  projects here\n")])}),[],!1,null,null,null);e.default=component.exports}}]);
