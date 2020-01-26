ec399db082232d00aba031fdd2c1a59b2be2bf13
konny0311 <konotorie@gmail.com>
2020-01-26 21:41:10 +0900

    WIP index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,28 @@
 <template>
   <div>
-    home contents here
+    <v-row>
+      <v-col>
+        <h2>Konny0311</h2>
+        <v-img src="/myimg.jpg" />
+      </v-col>
+      <v-col>
+        <h2>What do I mainly code?</h2>
+        <li v-for="lang in langs">
+          {{ lang }}
+        </li>
+        <h2>What is my interest?</h2>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
 export default {
-  components: {}
+  components: {},
+  data() {
+    return {
+      langs: ['Python', 'Java Script']
+    }
+  }
 }
 </script>
