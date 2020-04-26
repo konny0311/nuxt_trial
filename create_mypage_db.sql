b9aeb0ff121123b021c7648c39dca621006cbae0
konny0311 <konotorie@gmail.com>
2020-04-26 15:26:17 +0900

    create dummy data

--- create_mypage_db.sql
+++ create_mypage_db.sql
@@ -1,5 +1,6 @@
-create table technology (id int not null, technology varchar(255) not null, primary key (id) );
-create table project (id int not null, name varchar(255) not null, summary varchar(255), primary key (id) );
-create table project_tech (id int not null, project_id int not null, technology_id int not null,
+create table technology (id int not null auto_increment, technology varchar(255) not null, primary key (id) );
+create table project (id int not null auto_increment, name varchar(255) not null, summary varchar(255), primary key (id) );
+create table project_tech (id int not null auto_increment, project_id int not null, technology_id int not null,
+																	primary key (id),
 																	foreign key (project_id) references project(id),
 																	foreign key (technology_id) references technology(id) );
---
+++ dummy_data.sql
@@ -0,0 +1,13 @@
+insert into technology (technology) values ('Python'), 
+																					 ('Java Script'),
+																					 ('Java'),
+																					 ('AWS'),
+																					 ('OpenCV'),
+																					 ('TensorFlow'),
+																					 ('Keras'),
+																					 ('Ubuntu');
+insert into project (name, summary) values ('Expiry date recognition',
+																						'Read expiry date on a card box using Google Cloud Vision from an image.');
+insert into project_tech (project_id, technology_id) values (1, 3),
+																														(1, 4),
+																														(1, 5);
